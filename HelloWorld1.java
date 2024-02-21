24201f8319ead67a8c9dd634715e503e40a00774
Jimmy22051 <jimmy22051@gmail.com>
2024-02-21 14:57:43 +0800

    新增宣告

---
+++ HelloWorld1.java
@@ -0,0 +1,11 @@
+public class HelloWorld{
+
+    public static void main(String[] args){
+      //this is a method
+      /*jimmy*/
+
+          System.out.println("Hello World");
+          int salary;
+      }
+
+      }
